3fea8a89bdb4854b6e1cb156df41393b4f3dc578
Bill Lowe <bill.lowe87@gmail.com>
2024-11-06 23:17:35 +0000

    TriggerWithTagsFilter.cs

---
+++ Runtime/Helper Components/ColliderEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LowEndGames.ObjectTagSystem
+{
+    [Serializable]
+    public class ColliderEvent : UnityEvent<Collider> {}
+}
---
+++ Runtime/Helper Components/TriggerWithTagsFilter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace LowEndGames.ObjectTagSystem
+{
+    public class TriggerWithTagsFilter : MonoBehaviour
+    {
+        [SerializeField] private TagsFilter m_filter = new TagsFilter();
+        [SerializeField] private bool m_ignoreTriggers;
+        [SerializeField] private ColliderEvent m_onEnter;
+        [SerializeField] private ColliderEvent m_onExit;
+    
+        public ColliderEvent OnEnter => m_onEnter;
+        public ColliderEvent OnExit => m_onExit;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.isTrigger && m_ignoreTriggers)
+                return;
+            
+            if (IsValidCollider(other))
+            {
+                m_onEnter.Invoke(other);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.isTrigger && m_ignoreTriggers)
+                return;
+            
+            if (IsValidCollider(other))
+            {
+                m_onExit.Invoke(other);
+            }
+        }
+
+        private bool IsValidCollider(Collider other)
+        {
+            return other.TryGetComponent<TaggedObject>(out var taggedObject) && m_filter.Check(taggedObject);
+        }
+    }
+}
